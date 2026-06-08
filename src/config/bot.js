f019f4553e1cb422d2504eda4ec3ae651cd354d7
vaosamau-collab <vaosamau@gmail.com>
2026-06-08 20:21:36 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made with im tired ",
         // Activity type number (0 = Playing).
         type: 0, 
       },
